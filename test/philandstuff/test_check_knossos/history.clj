a208e77fdb2bfc4d97a95b75994f1d1d05f2acea
Philip Potter <philip.g.potter@gmail.com>
2014-06-30 22:15:21 +0100

    simplify history recording
    knossos.core/analysis calls knossos.history/complete, so us consumers
    don't need to fill in the :invoke calls with the correct :values.

--- test/philandstuff/test_check_knossos/history.clj
+++ test/philandstuff/test_check_knossos/history.clj
@@ -6,11 +6,11 @@
   returning the recorded history
 
   (sequential-history stack {:push push-fn, :pop pop-fn}
-                            [:push :push :pop])
+                            [[:push 4] [:push 5] [:pop]])
 "
-  [machine actions test-case]
+  [target actions test-case]
   (mapcat (fn [[action & args]]
-            (let [value (apply (actions action) machine args)]
+            (let [value (apply (actions action) target args)]
               [(op/invoke :solo action value)
                (op/ok     :solo action value)]))
           test-case))
@@ -21,19 +21,12 @@
 Wraps a fn to record invocations and successful returns in a
   knossos history atom.  The atom should contain a vector, which will
   have new history items conjed onto it.  id is a keyword that
-  represents this fn in the knossos history.
-
-  The :invoke record is conjed before the function is called, but at
-  that time the return value is not known and recorded as nil; once
-  the fn returns, the :ok record is conjed, and the :invoke record is
-  updated to indicate the actual value returned."
+  represents this fn in the knossos history."
   [actions [op & args] history]
-  (fn [me machine]
-    (let [history-start (swap! history conj (op/invoke me op nil))
-          invoke-index  (dec (count history-start))
-          value         (apply (actions op) machine args)]
-      (swap! history conj (op/ok me op value))
-      (swap! history assoc-in [invoke-index :value] value))))
+  (fn [me target]
+    (let [_     (swap! history conj (op/invoke me op nil))
+          value (apply (actions op) target args)]
+      (swap! history conj (op/ok me op value)))))
 
 (defn recorded-parallel-history
   "Executes in parallel a number of series of actions against a unit
@@ -44,11 +37,11 @@ Wraps a fn to record invocations and successful returns in a
 
   (recorded-parallel-history obj {:take take-fn, :put put-fn}
                                  {:t1 [:take :take] :t2 [:take :put]})"
-  [machine actions test-case]
+  [target actions test-case]
   (let [history   (atom [])
         processes (for [[process-id ops] test-case]
                     (fn []
                       (doseq [op (map #(wrap-fn-record-history actions % history) ops)]
-                        (op process-id machine))))]
+                        (op process-id target))))]
     (dorun (map deref (.invokeAll clojure.lang.Agent/soloExecutor processes)))
     @history))
